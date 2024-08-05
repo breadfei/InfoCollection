925267cbf77c266d71b48733b756f90169ef346f
Brady <hartmanbrady1110@gmail.com>
2024-08-05 14:01:12 -0400

    first commit

---
+++ Day 1 Project/Program.cs
@@ -0,0 +1,28 @@
+﻿/* ask the user for their personal info
+if they are an adult
+ - HAHA Took your bank info
+Not an adult
+ -Don't give out your personal info
+*/
+
+Console.WriteLine("Welcome to info collector.");
+
+//collecting info
+Console.WriteLine("Please type out your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}, what is your age?");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{name} what is your hometown?");
+string hometown = Console.ReadLine();
+
+//display results
+if (age >= 18)
+{
+    Console.WriteLine($"HAHA {name}, I am going to take your account that is in {hometown}");
+}
+else
+{
+    Console.WriteLine($"{name} don't give out your info.");
+}
